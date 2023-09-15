3374802f6d3be694209dac9a1f9395c1fa0e33e2
AL-Amin008 <mamin.193008@bscse.uiu.ac.bd>
2023-09-15 12:12:54 +0600

    second

---
+++ script.js
@@ -0,0 +1,19 @@
+const jokeText = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+const getJokeFunc = async () => {
+    const config = {
+        headers: {
+            Accept: "application/json",
+        }
+    }
+    const res = await fetch("https://icanhazdadjoke.com", config);
+    const data = await res.json();
+
+    console.log(data);
+
+    jokeText.textContent = data?.joke;
+}
+
+getJokeFunc();
+jokeBtn.addEventListener("click",getJokeFunc);
